ce81205868f1b9e2f0b4d049b927ea60d686d495
nana845 <abdulbarisahil2@gmail.com>
2025-05-29 22:34:53 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BINANCE_API_KEY = "ستاسو_API_Key"
-BINANCE_SECRET_KEY = "ستاسو_Secret_Key"
-TELEGRAM_BOT_TOKEN = "ستاسو_Telegram_Bot_Token"
-TELEGRAM_USER_ID = "ستاسو_User_ID"  # یوازې تاسو ته لاسرسی ورکول
+BINANCE_API_KEY="ستاسو_اصلي_API_Key"
+BINANCE_SECRET_KEY="ستاسو_اصلي_Secret_Key"
+TELEGRAM_BOT_TOKEN="ستاسو_Telegram_Bot_Token"
+TELEGRAM_USER_ID="ستاسو_User_ID"
