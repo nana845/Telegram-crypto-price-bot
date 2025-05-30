fc2c50488f825d40fd0da61014fe3b979d0b84da
nana845 <abdulbarisahil2@gmail.com>
2025-05-30 05:20:24 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BINANCE_API_KEY="ستاسو_اصلي_API_Key"
-BINANCE_SECRET_KEY="ستاسو_اصلي_Secret_Key"
+BINANCE_API_KEY="ستاسو_اصلي_API_کی"
+BINANCE_SECRET_KEY="ستاسو_Secret_Key"
 TELEGRAM_BOT_TOKEN="ستاسو_Telegram_Bot_Token"
 TELEGRAM_USER_ID="ستاسو_User_ID"
